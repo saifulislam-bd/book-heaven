311f49449cfff0a9c83e120ec90b6fe6e3fa41fb
saifulislam-bd <saifulislam.github@gmail.com>
2024-06-18 21:30:37 +0600

    add Book api

---
+++ server/models/Book.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+// Declare the Schema of the Mongo model
+const bookSchema = new mongoose.Schema(
+  {
+    url: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    language: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+//Export the model
+export default mongoose.model("Book", bookSchema);
