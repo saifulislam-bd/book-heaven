4bc121cd25f8ec7534a71f393a2c88d6fbea7dfc
saifulislam-bd <saifulislam.github@gmail.com>
2024-06-21 21:42:37 +0600

    add book update and delete api

--- server/controllers/bookController.js
+++ server/controllers/bookController.js
@@ -26,3 +26,40 @@ export const addBook = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
+//update book (admin)
+export const updateBook = async (req, res) => {
+  try {
+    const { bookid } = req.headers;
+    const updatedBook = await Book.findByIdAndUpdate(
+      bookid,
+      {
+        url: req.body.url,
+        title: req.body.title,
+        author: req.body.author,
+        price: req.body.price,
+        description: req.body.description,
+        language: req.body.language,
+      },
+      { new: true }
+    );
+    return res
+      .status(200)
+      .json({ message: "Book was updated successfully", updatedBook });
+  } catch (error) {
+    res.status(500).json({ message: "An error occurred" });
+  }
+};
+
+// delete book (admin)
+export const deleteBook = async (req, res) => {
+  try {
+    const { bookid } = req.headers;
+    const deletedBook = await Book.findByIdAndDelete(bookid);
+    return res
+      .status(200)
+      .json({ message: "Book was deleted successfully", deletedBook });
+  } catch (error) {
+    res.status(500).json({ message: "An error occurred" });
+  }
+};
--- server/routes/bookRoute.js
+++ server/routes/bookRoute.js
@@ -1,8 +1,14 @@
 import express from "express";
 import authenticateToken from "../utils/userAuth.js";
-import { addBook } from "../controllers/bookController.js";
+import {
+  addBook,
+  deleteBook,
+  updateBook,
+} from "../controllers/bookController.js";
 const router = express.Router();
 
 router.post("/add-book", authenticateToken, addBook);
+router.put("/update-book", authenticateToken, updateBook);
+router.delete("/delete-book", authenticateToken, deleteBook);
 
 export default router;
