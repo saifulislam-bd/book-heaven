3173bbc1ad975582d6fbdc154ea8a924bd0f5cd8
saifulislam-bd <saifulislam.github@gmail.com>
2024-06-23 22:05:24 +0600

    add remove-book-from-favList api

--- server/controllers/favoriteController.js
+++ server/controllers/favoriteController.js
@@ -21,3 +21,23 @@ export const addBookToFavorite = async (req, res) => {
     return res.status(500).json({ message: "An error occurred" });
   }
 };
+
+//remove book from favorite
+export const removeBookFromFavorite = async (req, res) => {
+  try {
+    const { bookid, id } = req.headers;
+    const userData = await User.findById(id);
+    const isBookFavorite = userData.favorites.includes(bookid);
+
+    if (isBookFavorite) {
+      await User.findByIdAndUpdate(id, {
+        $pull: { favorites: bookid },
+      });
+    }
+    return res.status(200).json({
+      message: "Book is removed from the favorite list.",
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "An error occurred" });
+  }
+};
--- server/routes/favoriteRoute.js
+++ server/routes/favoriteRoute.js
@@ -1,7 +1,16 @@
 import express from "express";
 const router = express.Router();
 import authenticateToken from "../utils/userAuth.js";
-import { addBookToFavorite } from "../controllers/favoriteController.js";
+import {
+  addBookToFavorite,
+  removeBookFromFavorite,
+} from "../controllers/favoriteController.js";
 
 router.put("/add-book-to-favorite", authenticateToken, addBookToFavorite);
+router.delete(
+  "/remove-book-from-favorite",
+  authenticateToken,
+  removeBookFromFavorite
+);
+
 export default router;
