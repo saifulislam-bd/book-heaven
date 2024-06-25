392c3de9d8ceaf488699e72c411c52966e01e913
saifulislam-bd <saifulislam.github@gmail.com>
2024-06-25 20:52:01 +0600

    add add-to-cart api

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -1,1 +1,20 @@
 import User from "../models/User.js";
+
+// add to cart
+export const addToCart = async (req, res) => {
+  try {
+    const { bookid, id } = req.headers;
+    const userData = await User.findById(id);
+    const isBookInCart = userData.cart.includes(bookid);
+    if (isBookInCart) {
+      return res.json({
+        status: "Success",
+        message: "Book is already in cart.",
+      });
+    }
+    await User.findByIdAndUpdate(id, { $push: { cart: bookid } });
+    return res.json({ status: "Success", message: "Book is added to cart." });
+  } catch (error) {
+    return res.status(500).json({ message: "An error occurred" });
+  }
+};
--- server/routes/cartRoute.js
+++ server/routes/cartRoute.js
@@ -1,7 +1,8 @@
 import express from "express";
 const router = express.Router();
 import authenticateToken from "../utils/userAuth.js";
+import { addToCart } from "../controllers/cartController.js";
 
-router.post("/add-to-cart", authenticateToken, addToCart);
+router.put("/add-to-cart", authenticateToken, addToCart);
 
 export default router;
